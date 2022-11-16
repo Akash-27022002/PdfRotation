8862bbda8da4f8248fcc14d4d8cfc1539106473e
Akash Sachan <65087180+Akash-27022002@users.noreply.github.com>
2022-11-16 12:49:52 +0530

    update return type

--- pythonProject/PDFRoation/api/views.py
+++ pythonProject/PDFRoation/api/views.py
@@ -10,27 +10,31 @@ def api_home(request , *args ,**kwargs):
     angle_rotation = data["angle_of_rotation"]
     page_number = data["page_number"]
     '''
-    "file_path" : "C:/Users/hacke/Downloads/Crypto Ass1.pdf",
+    {
+    "file_path" : "C:/Users/hacke/Downloads/crypto 1.pdf",
     "angle_of_rotation":90,
     "page_number":2
+    }
     '''
-    helper(file_path,angle_rotation,page_number-1)
+    output_filePath =  helper(file_path,angle_rotation,page_number-1)
     return Response(
         {
-            "msg": file_path
+            "msg": "succes",
+            "file_Path":output_filePath
         }
     )
 
 
 def helper(file_path,ang,page_num):
     target_file = file_path
-    output_file = open(file_path[0:file_path.rfind('/')+1]+'output_rotated.pdf', 'wb')
+    output_filePath = file_path[0:file_path.rfind('/')+1]+'output_rotated.pdf'
+    output_file = open(output_filePath, 'wb')
     print(output_file)
     pdf = PdfFileReader(target_file)
     writer = PdfFileWriter()
 
     num_page = pdf.getNumPages()
-    print(num_page,"slkjslkj",page_num)
+    # print(num_page,"slkjslkj",page_num)
     angle = ang
     rotated_page = page_num
 
@@ -43,3 +47,4 @@ def helper(file_path,ang,page_num):
     writer.write(output_file)
 
     output_file.close()
+    return output_filePath
